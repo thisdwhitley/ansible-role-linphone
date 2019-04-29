f4ac21d0e99859c0309637f1d95fc5c062628c5f
thisdwhitley <thisdwhitley@gmail.com>
2019-04-29 09:08:26 -0400

    updating because my tests were bad

--- molecule/default/tests/test_config.py
+++ molecule/default/tests/test_config.py
@@ -13,11 +13,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 #   test_usr2: irc_nick, servers, and channels specified
 #   test_usr3: network_name, and servers specified
 @pytest.mark.parametrize('file, content', [
-    ("/home/test_usr2/.var/app/io.github.Hexchat/config/hexchat/hexchat.conf", "testUser2"),
-    ("/home/test_usr2/.var/app/io.github.Hexchat/config/hexchat/servlist.conf", "6667"),
-    ("/home/test_usr2/.var/app/io.github.Hexchat/config/hexchat/servlist.conf", "noob"),
-    ("/home/test_usr3/.var/app/io.github.Hexchat/config/hexchat/servlist.conf", "ZNC"),
-    ("/home/test_usr3/.var/app/io.github.Hexchat/config/hexchat/servlist.conf", "6667"),
+    ("/home/test_usr1/.var/app/com.belledonnecommunications.linphone/config/linphone/linphonerc", "test_usr1"),
+    ("/home/test_usr1/.var/app/com.belledonnecommunications.linphone/config/linphone/linphonerc", "1234567890"),
+    ("/home/test_usr1/.var/app/com.belledonnecommunications.linphone/config/linphone/linphonerc", "S3cr3t"),
+    ("/home/test_usr1/.var/app/com.belledonnecommunications.linphone/config/linphone/linphonerc", "sip.network.com"),
+    ("/home/test_usr1/.var/app/com.belledonnecommunications.linphone/config/linphone/linphonerc", "0123456789"),
 ])
 def test_contents(host, file, content):
     file = host.file(file)
--- molecule/default/tests/test_install.py
+++ molecule/default/tests/test_install.py
@@ -6,6 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hexchat_installed(host):
+def test_linphone_installed(host):
     # I used to check with hexchat.is_installed, that doesn't work with flatpak
-    assert host.check_output('flatpak list') != "linephone"
+    assert host.check_output('flatpak info com.belledonnecommunications.linphone')
